4302407bb3c9d31cd9734b905c39c684c0479431
Neftaly Luis <neftaly.luis@resuelve.mx>
2018-08-29 18:56:35 -0500

    Autofix de Rubocop

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: true
+
 class LeadsController < ApplicationController
-  def index
-  end
+  def index; end
 
   def create
     @lead = Lead.new(params[:lead].permit(:name, :email))
     if @lead.save
-      render action: "index", saved: true
+      render action: 'index', saved: true
     else
-      render action: "index", errors: @lead.errors
+      render action: 'index', errors: @lead.errors
     end
   end
 end
--- db/migrate/20180623171651_create_leads.rb
+++ db/migrate/20180623171651_create_leads.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateLeads < ActiveRecord::Migration[5.2]
   def change
     create_table :leads do |t|
