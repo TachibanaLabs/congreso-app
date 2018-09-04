94afb720cea73d6c826ab0a413be1c65dad8e057
Neftaly Luis <abi@airmail.cc>
2018-09-03 21:05:49 -0500

    Rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,36 +12,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_04_010645) do
-
-  create_table "leads", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "title", limit: 60, default: "", null: false
-    t.string "career", limit: 60, default: "", null: false
-    t.string "institution", limit: 60, default: "", null: false
-    t.string "address", limit: 60, default: "", null: false
-    t.string "city", limit: 60, default: "", null: false
-    t.string "postal_code", limit: 60, default: "", null: false
-    t.string "country", limit: 60, default: "", null: false
-    t.string "phone_number", limit: 60, default: "", null: false
-    t.boolean "student", default: false
-    t.integer "paper"
-    t.integer "symposium"
-    t.integer "table_discussion"
+ActiveRecord::Schema.define(version: 20_180_904_010_645) do
+  create_table 'leads', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'email', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'title', limit: 60, default: '', null: false
+    t.string 'career', limit: 60, default: '', null: false
+    t.string 'institution', limit: 60, default: '', null: false
+    t.string 'address', limit: 60, default: '', null: false
+    t.string 'city', limit: 60, default: '', null: false
+    t.string 'postal_code', limit: 60, default: '', null: false
+    t.string 'country', limit: 60, default: '', null: false
+    t.string 'phone_number', limit: 60, default: '', null: false
+    t.boolean 'student', default: false
+    t.integer 'paper'
+    t.integer 'symposium'
+    t.integer 'table_discussion'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", null: false
-    t.string "encrypted_password", limit: 128, null: false
-    t.string "confirmation_token", limit: 128
-    t.string "remember_token", limit: 128, null: false
-    t.index ["email"], name: "index_users_on_email"
-    t.index ["remember_token"], name: "index_users_on_remember_token"
+  create_table 'users', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'email', null: false
+    t.string 'encrypted_password', limit: 128, null: false
+    t.string 'confirmation_token', limit: 128
+    t.string 'remember_token', limit: 128, null: false
+    t.index ['email'], name: 'index_users_on_email'
+    t.index ['remember_token'], name: 'index_users_on_remember_token'
   end
-
 end
